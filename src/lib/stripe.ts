bc8676dc59fb36cd77d48def91a80cb30d9a7070
brodel10 <rodeell21@gmail.com>
2025-08-07 12:56:41 -0700

    diff

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,4 +2,8 @@ import "server-only";
 
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "sk_test");
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY ||
+    process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY ||
+    "sk_test"
+);
