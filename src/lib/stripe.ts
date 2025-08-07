dbdeb166e7375d2081c28ee3d8327461c7f526d1
brodel10 <rodeell21@gmail.com>
2025-08-07 11:23:10 -0700

    this

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,6 +2,9 @@ import "server-only";
 
 import Stripe from "stripe";
 
+if (process.env.NODE_ENV === "production" && !process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing SERVICE_API_KEY");
+}
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: "2025-07-30.basil",
 });
