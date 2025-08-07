683053b08e75f831eb6d9e32fedafe3c8738a7ff
brodel10 <rodeell21@gmail.com>
2025-08-07 12:04:56 -0700

    1;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Checkout from "./components/checkout";
+// import Checkout from "./components/checkout";
 
 export default function Page() {
   return (
     <div id="checkout">
-      <Checkout />
+      {/* <Checkout /> */}
+      HI
     </div>
   );
 }
