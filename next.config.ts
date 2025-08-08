813a7fe351174fa1888c6e14600d1d8a90235d3b
brodel10 <rodeell21@gmail.com>
2025-08-08 11:24:37 -0700

    added origins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: "/app",
+  experimental: {
+    serverActions: {
+      // allow both your Webflow domain and the internal cosmic.services host
+      allowedOrigins: [
+        "playground-ff532a.webflow.io",
+        "1165bbfa-b77d-4f9a-8efd-04a347180483.wf-app-prod.cosmic.webflow.services",
+      ],
+    },
+  },
 };
 
 export default nextConfig;
